bff600d01fd5b37f77dd5f31543d47d7ff245d09
rehanchanija <rehanchanija@gmail.com>
2024-09-23 00:04:20 +0530

    about section

---
+++ pages/aboutus.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="">
+      <section className="text-gray-600 body-font min-h-[70vh]">
+        <div className="container px-5 py-10 mx-auto">
+          <div className="text-center mb-10">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font text-black mb-4">
+              About Us
+            </h1>
+            {/* <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
+
+            </p> */}
+            <div className="flex mt-4 justify-center">
+              <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex" />
+            </div>
+          </div>
+          <ul className="text-lg font-semibold text-center max-w-5xl mx-auto text-black">
+            <li>
+            Dreamz 24 - Events & Entertainment Solutions.
+            </li>
+            <br />
+            <li>
+            Get your dream wedding planned with Wedding Planner, the ultimate Indian Wedding Planning platform that offers you the best wedding vendors, photographers, and decorators along with their prices and reviews, all just a click away. If you&apos;re searching for wedding planners in India or the finest photographers, or simply need some ideas and inspiration, Wedding Planner is the perfect solution to all your wedding planning problems with its innovative features. Thanks to its comprehensive checklist, detailed vendor list, inspiring gallery, and blog, you can now plan your wedding efficiently without spending hours on it. Say goodbye to hours of wedding planning and hello to your dream wedding with our help!
+
+            </li>
+            <li>
+              Plan your wedding and events with us ,Get verified wedding vendors and a experienced event management team for your happy moments
+            </li>
+            {/* <li>we take our users' privacy seriously.</li>
+            <li>
+              Your Location: We use the location data to show you venues around
+              you. No data is saved on our servers.
+            </li>
+            <li>
+              Your Phone Number: We use the phone number to send you an OTP for
+              login. No data is saved on our servers.
+            </li>
+            <li>
+              Your application’s information [Retrieve running apps] & Logs: We
+              use this to gather the information that helps us fix issues that
+              you may experience with our app. We do not save any of this
+              information.
+            </li>
+            <li>
+              Your Accounts: We require this to be able to send Push
+              Notifications. Don’t worry, we never spam.
+            </li>
+            <li>
+              Your Contacts: We need access to your contacts so that you can
+              invite your friends to chat or join you after you've booked your
+              tickets. We ask for your contact permission for a bunch of
+              features like chat, sharing tickets with friends. Your information
+              will be kept confidentia
+            </li> */}
+          </ul>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default AboutUs;
