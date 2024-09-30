b1f3c50bcfd85cbfe8e1d530096a65a10bfd70b9
rehanchanija <rehanchanija@gmail.com>
2024-09-30 23:25:33 +0530

    Top section

---
+++ components/TopSearches.js
@@ -0,0 +1,221 @@
+import Link from "next/link";
+import React from "react";
+
+const TopSearches = () => {
+  return (
+    <section className="text-gray-600 body-font bg-white xl:px-0 px-4">
+      <div className="container  pb-10 pt-4 mx-auto">
+        <div className="flex w-full ">
+          <h1 className="text-2xl  pb-8 text-primary font-semibold">
+            Our Top Works
+          </h1>
+        </div>
+        <div className="flex flex-wrap -m-4">
+          <div className="p-4 md:w-1/3">
+            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+              <img
+                className="lg:h-72 md:h-48 w-full object-cover object-center"
+                src="/images/1.png"
+                alt="blog"
+              />
+              <div className="p-4">
+                <h1 className="title-font  font-semibold text-gray-900 mb-3 ">
+                  Carnival Theme
+                </h1>
+                <p className="leading-relaxed mb-3 text-sm">
+                  A Carnival theme event is the perfect way to liven up a party
+                  with a range of creative options for decorating &
+                  planning your event.
+                </p>
+                <div className="flex items-center flex-wrap ">
+                  <Link href="/category/birthday-party-planner">
+                    <p className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 hover:underline">
+                      Learn More
+                      <svg
+                        className="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path d="M5 12h14" />
+                        <path d="M12 5l7 7-7 7" />
+                      </svg>
+                    </p>
+                  </Link>
+
+                  <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                    <svg
+                      className="w-4 h-4 mr-1"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                      <circle cx={12} cy={12} r={3} />
+                    </svg>
+                    1.2K
+                  </span>
+                  <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                    <svg
+                      className="w-4 h-4 mr-1"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+                    </svg>
+                    6
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="p-4 md:w-1/3">
+            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+              <img
+                className="lg:h-72 md:h-48 w-full object-cover object-center"
+                src="/images/15.png"
+                alt="blog"
+              />
+              <div className="p-4">
+                <h1 className="title-font  font-semibold text-gray-900 mb-3">
+                Floral set-up for Haldi 
+                </h1>
+                <p className="leading-relaxed mb-3 text-sm">
+                  A haldi ceremony is usually what kickstarts the celebrations.
+                  Let us give you a heads-up on all you need to know about Haldi
+                  decorations.
+                </p>
+                <div className="flex items-center flex-wrap">
+                  <Link href="/category/event-planner">
+                    <p className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 hover:underline">
+                      Learn More
+                      <svg
+                        className="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path d="M5 12h14" />
+                        <path d="M12 5l7 7-7 7" />
+                      </svg>
+                    </p>
+                  </Link>
+                  <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                    <svg
+                      className="w-4 h-4 mr-1"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                      <circle cx={12} cy={12} r={3} />
+                    </svg>
+                    1.2K
+                  </span>
+                  <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                    <svg
+                      className="w-4 h-4 mr-1"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+                    </svg>
+                    6
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="p-4 md:w-1/3">
+            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+              <img
+                className="lg:h-72 md:h-48 w-full object-cover object-center"
+                src="/images/3.png"
+                alt="blog"
+              />
+              <div className="p-4">
+                <h1 className="title-font  font-semibold text-gray-900 mb-3">
+                  Elegant Stage Decoration
+                </h1>
+                <p className="leading-relaxed mb-3 text-sm">
+                  Stunning floral arrangements for weddings, parties, banquets,
+                  corporate events, and other special occasions.
+                </p>
+                <div className="flex items-center flex-wrap ">
+                  <Link href="/category/flower-decor">
+                    <p className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0 hover:underline">
+                      Learn More
+                      <svg
+                        className="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path d="M5 12h14" />
+                        <path d="M12 5l7 7-7 7" />
+                      </svg>
+                    </p>
+                  </Link>
+                  <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                    <svg
+                      className="w-4 h-4 mr-1"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                      <circle cx={12} cy={12} r={3} />
+                    </svg>
+                    1.2K
+                  </span>
+                  <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                    <svg
+                      className="w-4 h-4 mr-1"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+                    </svg>
+                    6
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TopSearches;
