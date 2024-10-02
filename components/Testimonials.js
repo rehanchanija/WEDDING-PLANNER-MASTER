c84ef665e72a08c27cac0a2e3eabbb8a6d2abb65
rehanchanija <rehanchanija@gmail.com>
2024-10-03 00:31:51 +0530

    p

---
+++ components/Testimonials.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div className='pb-6 w-full flex justify-center xl:px-0 px-4 bg-white'>
+    <div className=" xl:max-w-[90%] w-full flex flex-col">
+<div className="flex w-full ">
+<h1 className="text-2xl  pb-8 text-primary font-semibold">Testimonials</h1>
+</div>
+       <section className="text-gray-600 body-font">
+  <div className="container px-5 py-8 mx-auto">
+    <div className="flex flex-wrap -m-4">
+      <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+        <div className="h-full text-center">
+          <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2106&q=80" />
+          <p className="leading-relaxed">Fantastic team and fantastic arrangements by Dreams 24...
+I m from Delhi and came to Raipur for my sister&apos;s wedding ( Shalini weds Amit )Mr. Arjun Khandelwal is the one who took all worries and every single person who attended my wedding praised the arrangements of the wedding....
+</p>
+          <span className="inline-block h-1 w-10 rounded bg-primary mt-6 mb-4" />
+          <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">Vikrant Kaushik</h2>
+          {/* <p className="text-gray-500">Senior Product Designer</p> */}
+        </div>
+      </div>
+      <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+        <div className="h-full text-center">
+          <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://images.unsplash.com/photo-1583397758920-86c1cb75b8fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80" />
+          <p className="leading-relaxed">
+          Recommended; Amazing teamwork 👍 and prompt execution of groom-bride entry- on a very tight schedule. We had approached them, just 5-6 days before the wedding date. Guided us well through plans & other details and hence created life time memories. Thank you Dream24 team.🙏👏🏻👍
+
+          </p>
+          <span className="inline-block h-1 w-10 rounded bg-primary mt-6 mb-4" />
+          <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">Sonal Mishra</h2>
+          {/* <p className="text-gray-500">UI Develeoper</p> */}
+        </div>
+      </div>
+      <div className="lg:w-1/3 lg:mb-0 p-4">
+        <div className="h-full text-center">
+          <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://images.unsplash.com/photo-1508810301179-d8da058a3d29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&q=80" />
+          <p className="leading-relaxed">
+          Dreamz24 team - thank you for an amazing event and all your support. We recently had the privilege of their service for one of my cousin&apos;s weddings. They are a bunch of warm people, calm, know their work, and are truly professional. Special shout out to Arjun Khandelwal, his work is as charming as his personality.
+          </p>
+          <span className="inline-block h-1 w-10 rounded bg-primary mt-6 mb-4" />
+          <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">Manisha Khandelwal</h2>
+          {/* <p className="text-gray-500">CTO</p> */}
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
+
+    </div>
+  )
+}
+
+export default Testimonials
